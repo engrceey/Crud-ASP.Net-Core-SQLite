be88e10e6bbbde53e05292ddfae6341fdc8e4be1
engrceey <ogbondachristian@gmail.com>
2020-11-14 14:21:00 +0100

    initial commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+
+namespace Commander.Date
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+       public IEnumerable<Command> GetAppCommands() 
+       {
+           var commands = new List<Command>
+           {
+                new Command{Id=0, Howto="Boil an egg", Line ="Boil water", Platform = "kettle & Pan" },
+                new Command{Id=1, Howto="Boil an beans", Line ="help me", Platform = "youre not special" },
+                new Command{Id=2, Howto="fish", Line ="on God", Platform = "make money" }
+           };
+           return commands;
+       }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, Howto="Boil an egg", Line ="Boil water", Platform = "kettle & Pan" };
+        }
+
+    }
+}
